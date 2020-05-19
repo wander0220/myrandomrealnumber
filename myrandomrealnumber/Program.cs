4aebbbf64e6510aba3e8ce43dbd152589be6922b
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-19 12:55:33 +0900

    초기 커밋.

---
+++ myrandomrealnumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myrandomrealnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for(int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(0, 100);
+
+                Console.WriteLine(num);
+            }
+            
+            //while (true)
+            //{
+
+            //}
+        }
+    }
+}
